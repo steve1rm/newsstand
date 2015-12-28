4f2e912471a74c55ab604568c3a69e668127d7fa
steve <steve@sun-system.com>
2015-12-28 17:37:02 +0700

    initial commit

---
+++ app/src/main/java/androidbox/me/newsstand/MainActivity.java
@@ -0,0 +1,21 @@
+package androidbox.me.newsstand;
+
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, new MainFragment(), "MainFragment");
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        }
+    }
+}
