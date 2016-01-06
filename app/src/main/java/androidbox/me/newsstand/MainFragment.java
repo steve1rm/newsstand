c8188c1b3611b182616cd97a1271c9b06b146b63
steve <steve@sun-system.com>
2016-01-06 17:11:30 +0700

    Experimenting with different ways to create the image blue effect

--- app/src/main/java/androidbox/me/newsstand/MainFragment.java
+++ app/src/main/java/androidbox/me/newsstand/MainFragment.java
@@ -2,6 +2,11 @@ package androidbox.me.newsstand;
 
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,10 +34,58 @@ public class MainFragment extends Fragment {
         setHasOptionsMenu(true);
     }
 
+    @SuppressWarnings("deprecated")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        /* Set color of background image */
+        final ImageView ivBackground = (ImageView)view.findViewById(R.id.ivBackground);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inMutable = true;
+
+        Bitmap bitmapCouple = BitmapFactory.decodeResource(getResources(), R.drawable.couplePhoto, options);
+        Canvas canvas = new Canvas();
+        canvas.drawColor(getResources().getColor(R.color.colorBackgroundShape));
+        canvas.drawBitmap(bitmapCouple, new Matrix(), null);
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            // bitmap.eraseColor(getResources().getColor(R.color.colorBackgroundShape));
+        }
+        else {
+//            bitmap.eraseColor(getResources().getColor(R.color.colorBackgroundShape, null));
+        }
+
+        ivBackground.setImageBitmap(bitmapCouple);
+
+        /* Attempt 1 */
+        // ivBackground.setColorFilter(R.color.colorBackgroundShape, PorterDuff.Mode.DARKEN);
+        // ivBackground.setColorFilter(R.color.colorBackgroundShape);
+        ivBackground.setImageBitmap(bitmapCouple);
+
+        /* Attempt 2 */
+        // Bitmap bitmapCouple = BitmapFactory.decodeResource(getResources(), R.drawable.couplePhoto, options);
+        // bitmap.eraseColor(getResources().getColor(R.color.colorBackgroundShape));
+        ivBackground.setImageBitmap(bitmapCouple);
+
+        /* Attempt 3 */
+        /*Bitmap bitmapCouple = BitmapFactory.decodeResource(getResources(), R.drawable.couplePhoto, options);
+        Bitmap newBitmap = Bitmap.createBitmap(bitmapCouple.getWidth(), bitmapCouple.getHeight(), bitmapCouple.getConfig());
+        Canvas canvas = new Canvas(newBitmap);
+        canvas.drawColor(getResources().getColor(R.color.colorBackgroundShape));
+        canvas.drawBitmap(bitmapCouple, 0, 0, null);*/
+        ivBackground.setImageBitmap(bitmapCouple);
+
+        /* Attempt 4 */
+/*
+        Bitmap bitmapCouple = BitmapFactory.decodeResource(getResources(), R.drawable.couplePhoto, options);
+        Canvas canvas = new Canvas();
+        canvas.drawColor(getResources().getColor(R.color.colorBackgroundShape));
+        canvas.drawBitmap(bitmapCouple, new Matrix(), null);
+          ivBackground.setImageBitmap(bitmapCouple);
+*/
+
 
         final Toolbar toolbar = (Toolbar)view.findViewById(R.id.tool_bar);
         AppCompatActivity activity = (AppCompatActivity)getActivity();
